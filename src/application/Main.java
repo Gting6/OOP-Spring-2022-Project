98e39faf1c7d4fedcb013cf3d9beb8e5a9de1500
b08209032 <b08209032@ntu.edu.tw>
2022-04-24 23:07:07 +0800

    Basic: Fix main

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,5 +1,6 @@
 package application;
 	
+import control.Controller;
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
@@ -22,5 +23,8 @@ public class Main extends Application {
 	
 	public static void main(String[] args) {
 		launch(args);
+		Controller controller = new Controller();
+		controller.controlMain();
+		
 	}
 }
