b2e10d1cac4b2f485996882e4d4310484f568b38
Gting6 <b07901090@ntu.edu.tw>
2022-05-07 05:34:05 +0800

    FX: add signup success, flexible error, type select

--- src/application/LoginController.java
+++ src/application/LoginController.java
@@ -5,6 +5,8 @@ import java.net.URL;
 import java.util.ResourceBundle;
 import java.util.regex.Pattern;
 
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -13,6 +15,7 @@ import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
@@ -21,7 +24,7 @@ import model.Model;
 import model.UserType;
 import view.LoginView;
 
-public class LoginController extends Controller implements Initializable  {
+public class LoginController extends Controller implements Initializable {
 	private LoginView status;
 
 	@FXML
@@ -31,20 +34,49 @@ public class LoginController extends Controller implements Initializable  {
 	private Button btn2;
 
 	@FXML
+	private Label confirmLb;
+
+	@FXML
+	private Label wrongLb;
+
+	@FXML
+	private Label emailLb;
+
+	@FXML
+	private Label phoneLb;
+
+	@FXML
+	private Label nameLb;
+
+	@FXML
 	private TextField usernameTf;
 
 	@FXML
 	private TextField passwordTf;
-	
+
 	@FXML
 	private TextField confirmTf;
-	
+
 	@FXML
-	private Label confirmLb;
-	
+	private TextField nameTf;
+
 	@FXML
-	private Label wrongLb;
-	
+	private TextField phoneTf;
+
+	@FXML
+	private TextField emailTf;
+
+	@FXML
+	private ComboBox<String> typeCombo;
+
+	private UserType usertype;
+
+	// To set the error message for wrongLb
+	// Can be extend to input label.
+	private void setWrongLb(String s) {
+		wrongLb.setText(s);
+	}
+
 	// Helper function: render by status
 	@Override
 	protected void render() {
@@ -54,16 +86,29 @@ public class LoginController extends Controller implements Initializable  {
 			confirmTf.setVisible(false);
 			confirmLb.setVisible(false);
 			wrongLb.setVisible(false);
-		}
-		else if(status == LoginView.SignUp) {
+			emailTf.setVisible(false);
+			phoneTf.setVisible(false);
+			nameTf.setVisible(false);
+			emailLb.setVisible(false);
+			phoneLb.setVisible(false);
+			nameLb.setVisible(false);
+			typeCombo.setVisible(false);
+		} else if (status == LoginView.SignUp) {
 			btn1.setText("Go Back");
 			btn2.setText("Confirm");
 			confirmTf.setVisible(true);
 			confirmLb.setVisible(true);
 			wrongLb.setVisible(false);
+			emailTf.setVisible(true);
+			phoneTf.setVisible(true);
+			nameTf.setVisible(true);
+			emailLb.setVisible(true);
+			phoneLb.setVisible(true);
+			nameLb.setVisible(true);
+			typeCombo.setVisible(true);
 		}
 	}
-		
+
 	public void pressBtn1(ActionEvent event) throws IOException {
 		if (status == LoginView.Login) {
 			Model model = new Model();
@@ -71,27 +116,27 @@ public class LoginController extends Controller implements Initializable  {
 			passwordTf.getText();
 			// Add check who want to login? or use if to handle
 			String pattern = "[0-9a-zA-Z]+";
-			
+
 			while (true) {
-				if (!Pattern.matches(pattern, usernameTf.getText()) || !Pattern.matches(pattern, passwordTf.getText())) {
-					// TODO change the errormsg
-					System.out.println("Error pattern in username or password");
+				if (!Pattern.matches(pattern, usernameTf.getText())
+						|| !Pattern.matches(pattern, passwordTf.getText())) {
+					// TODO (done) change the errormsg
+					setWrongLb("Error pattern in username or password");
 					wrongLb.setVisible(true);
 					// refresh
 					break;
 				}
-				
+
 				if (model.checkMemberLoginIn(usernameTf.getText(), passwordTf.getText())) {
 					switchScene(ViewEnum.MEMBER, event, usernameTf.getText());
 					System.out.println("Login success");
-				}
-				else {
-					System.out.println("Login fail");
+				} else {
+					setWrongLb("Login Fail");
 					wrongLb.setVisible(true);
 				}
-				
+
 				break;
-				
+
 				// TODO [BackEnd] check restaurant and deliver
 //				case "restaurant":
 //				switchScene(ViewEnum.RESTAURANT, event, username);
@@ -99,80 +144,95 @@ public class LoginController extends Controller implements Initializable  {
 //				case "deliver":
 //				switchScene(ViewEnum.DELIVER, event, username);
 //				break;
-				
-			}
-			
 
+			}
 
 		}
-		
+
 		// When the status is SignUp, btn1 becomes "back" button.
 		else if (status == LoginView.SignUp) {
 			status = LoginView.Login;
 			render();
 		}
 	}
-	
+
 	public void pressBtn2(ActionEvent event) throws IOException {
 		if (status == LoginView.Login) {
 			status = LoginView.SignUp;
 			render();
-		}
-		else if (status == LoginView.SignUp) {
+		} else if (status == LoginView.SignUp) {
 			Model model = new Model();
 			usernameTf.getText();
 			passwordTf.getText();
 			confirmTf.getText();
-			
-			// TODO [FX] need add the Tf of email 
-			String email = "abcd@gmail.com";
-			
-			// TODO [FX] need add the Tf of phoneNumber
-			String address = "0922323232";
-			
-			// TODO [FX] need add the Tf of name
-			String name = "";
-						
+
+			// TODO [FX](done) need add the Tf of email
+			String email = emailTf.getText();
+
+			// TODO [FX](done) need add the Tf of phoneNumber
+			String address = phoneTf.getText();
+
+			// TODO [FX](done) need add the Tf of name
+			String name = nameTf.getText();
+
 			String pattern = "[0-9a-zA-Z]+";
-			
+
 			System.out.println(passwordTf.getText());
 			System.out.println(confirmTf.getText());
 
+			// TODO (done) [FX] Need a <select> in css to check which type(member, rest,
+			// deliver...) the user want to sign-up.
+			// usertype = UserType.Member;
+			switch (typeCombo.getValue()) {
+			case "Member":
+				usertype = UserType.Member;
+				break;
+			case "Deliver":
+				usertype = UserType.Deliver;
+				break;
+			case "Restaurant":
+				usertype = UserType.Restaurant;
+				break;
+			default:
+				usertype = UserType.Error;
+				break;
+			}
+
 			while (true) {
-				
-			
-				if (!Pattern.matches(pattern, usernameTf.getText()) || !Pattern.matches(pattern, passwordTf.getText())) {
-					// TODO [Fx] The string in wrongLb can be change to the string mentioned below.
-					System.out.println("Error pattern in username or password");
+
+				if (!Pattern.matches(pattern, usernameTf.getText())
+						|| !Pattern.matches(pattern, passwordTf.getText())) {
+					// TODO [Fx](done) The string in wrongLb can be change to the string mentioned
+					// below.
+					setWrongLb("Error pattern in username or password");
 					wrongLb.setVisible(true);
 					break;
 				}
-				
+
 				if (!passwordTf.getText().equals(confirmTf.getText())) {
-					// TODO [Fx] The string in wrongLb can be change to the string mentioned below.
-					System.out.println("Error password and confirm password");
+					// TODO [Fx](done) The string in wrongLb can be change to the string mentioned
+					// below.
+					setWrongLb("Error password and confirm password");
 					wrongLb.setVisible(true);
 					break;
 				}
-				
-				// TODO [FX] Need a <select> in css to check which type(member, rest, deliver...) the user want to sign-up.
-				// usertype = UserType.Member;
-				
+
 				Member member = new Member(usernameTf.getText(), passwordTf.getText(), email, address, name);
 				if (!model.checkMemberInWhenRegister(member.getUserName(), member.getPassword())) {
 					model.addMember(member);
-					// TODO [FX] Success, show sign-up success, and give a button to back to login page.
+					// TODO (done) [FX] Success, show sign-up success, and give a button to back to
+					// login
+					// page.
+					switchScene(ViewEnum.SIGNUPSUCCESS, event, member.getUserName());
 					System.out.println(model.checkMemberInWhenRegister(member.getUserName(), member.getPassword()));
-					switchScene(ViewEnum.MEMBER, event, member.getUserName());
-				}
-				else {
-					// TODO [Fx] The string in wrongLb can be change to the string mentioned below.
-					System.out.println("fail to register");
+					// switchScene(ViewEnum.MEMBER, event, member.getUserName());
+				} else {
+					// TODO (done) [Fx] The string in wrongLb can be change to the string mentioned
+					// below.
+					setWrongLb("Fail to register");
 					wrongLb.setVisible(true);
 				}
-				
 				break;
-
 			}
 		}
 
@@ -182,6 +242,7 @@ public class LoginController extends Controller implements Initializable  {
 	public void initialize(URL arg0, ResourceBundle arg1) {
 		// TODO Auto-generated method stub
 		status = LoginView.Login;
+		typeCombo.getItems().setAll("Member", "Deliver", "Restaurant"); // set the options
 		render();
 	}
 }
--- src/module-info.java
+++ src/module-info.java
@@ -3,6 +3,7 @@ module OOP {
 	requires javafx.fxml;
 	requires javafx.graphics;
 	requires java.sql;
+	requires javafx.base;
 	
 	opens application to javafx.graphics, javafx.fxml;
 }
