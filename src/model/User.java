b126c80f94eb0da0af962f46c2d71022bf8a733b
b08209032 <b08209032@ntu.edu.tw>
2022-05-13 01:02:11 +0800

    Basic: LoginConTroller

--- src/model/User.java
+++ src/model/User.java
@@ -13,6 +13,11 @@ public abstract class User {
 	public User() {
 
 	}
+	
+	public User(String username) {
+		this.username = username;
+
+	}
 
 	public User(String username, String password) {
 		this.username = username;
